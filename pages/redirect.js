c1dbcad70d522b5903e8b4d6bf8eef39c935022e
Mathias <mathiasnouzy@gmail.com>
2024-10-10 19:32:03 +0200

    test: redirection

---
+++ pages/redirect.js
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const RedirectPage = () => {
+  const router = useRouter();
+  const { slug } = router.query;  // Pour obtenir le {slug} dynamique
+
+  useEffect(() => {
+    const isInAppBrowser = () => {
+      const ua = navigator.userAgent || navigator.vendor || window.opera;
+      // Détection des in-app browsers (Instagram, Facebook, Messenger, etc.)
+      return /instagram|fb_iab|fb4a|messenger/i.test(ua);
+    };
+
+    if (isInAppBrowser()) {
+      // Si c'est un in-app browser, rediriger vers le navigateur par défaut
+      window.location.href = `https://snouzy.com`;
+    } else {
+      // Redirection classique
+      window.location.href = `https://snouzy.com`;
+    }
+  }, [slug]);
+
+  return <p>Redirection en cours...</p>;
+};
+
+export default RedirectPage;
